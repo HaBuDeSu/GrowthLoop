2023fb61966a57784ade865f71bd5cc44de78d61
Ken Ruf <ruf.kenichi@gmail.com>
2020-03-11 19:45:05 -0500

    login successful

--- growthloop/src/components/Login.js
+++ growthloop/src/components/Login.js
@@ -4,13 +4,28 @@ import axios from 'axios';
 
 const Login = props => {
 
-    let [creds, setCreds] = useState({
+    let [credentials, setCredentials] = useState({
         email: "",
         password: ""
     })
 
-    const login = async () => {
-        await axios.post('https://growthloop.herokuapp.com/api/users/login', creds)
+    const [message, setMessage] = useState("")
+
+    const handleChange = event => {
+        setCredentials({
+            ...credentials,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const login = async event => {
+        event.preventDefault()
+        await axios.post('https://growthloop.herokuapp.com/api/users/login', credentials)
+            .then(res => {
+                localStorage.setItem("token", res.data.token)
+                props.history.push("/dashboard")
+            })
+            .catch(err => setMessage("Invalid Credentials"))
     }
 
     return (
@@ -20,13 +35,15 @@ const Login = props => {
                 <FormText>Login To Your Account</FormText>
                 <FormGroup>
                     <Label>Email</Label>
-                    <Input></Input>
+                    <Input onChange={handleChange} name="email"></Input>
                 </FormGroup>
                 <FormGroup>
                     <Label>Password</Label>
-                    <Input></Input>
+                    <Input type="password" onChange={handleChange} name="password"></Input>
                 </FormGroup>
+                <Button>Login</Button>
             </Form>
+            <p>{message}</p>
         </div>
     )
 }
